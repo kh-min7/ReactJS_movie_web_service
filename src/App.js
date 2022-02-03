fa18eefab7c11eee4a9faa6cc10bcd6f97f18c7a
kh-min7 <rudgh2879@naver.com>
2022-02-03 19:21:22 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("render");
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
 
 export default App;
+
+// const [counter, setValue] = useState(0);
+// -> 리액트 앱으로 하는 중이라 앞에 React 안붙여도됨
+
+// state를 변경할 때 ‘모든’ code 들을 항상 다시 실행됨
+// 첫 번째 render에만 코드가 실행되고 다른 state변화에는 실행되지 않도록 만들자
+// 예) API를 통해 데이터를 가져올 때 컴포넌트 렌더에서 API를 부르고
+// 이후 상태가 변화할 때 그 API에서 데이터를 다시 가져오지 않게 만들 수 있다.
--- src/Button.js
+++ src/Button.js
@@ -10,15 +10,3 @@ Button.propTypes = {
 };
 
 export default Button;
-
-// css 코드를 자바스크립트 객체로 변환시켜줌
-// import styles from "./~";
-
-// styles가 css 코드를 가지고 있는 객체로 넘어옴
-// 따라서 해당 css코드에 작성된 class name(여기서는 btn)을 프로퍼티 접근 연산자(.)를 사용해서 이용가능해짐.
-
-// < Button style={styles.btn} / >
-// 위와 같이 작성해서 해당 css 스타일링을 사용할 수 있음.
-
-// 브라우저를 통해 html 코드를 확인해보면 해당 컴포넌트에 무작위의 class name이 붙음.
-// 요소가 각각의 클래스네임을 가지게 돼서 일일이 class name을 기억해서 스타일링 할 필요가 없음
