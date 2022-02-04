7c1940de6de176a6b9b76c6a7ea58e2715e5ae3a
kh-min7 <rudgh2879@naver.com>
2022-02-04 13:22:00 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Button from "./Button";
 import styles from "./App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("render");
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("CALL THE API");
+  }, []);
   return (
     <div>
       <h1>{counter}</h1>
@@ -16,10 +19,13 @@ function App() {
 
 export default App;
 
-// const [counter, setValue] = useState(0);
-// -> 리액트 앱으로 하는 중이라 앞에 React 안붙여도됨
-
-// state를 변경할 때 ‘모든’ code 들을 항상 다시 실행됨
-// 첫 번째 render에만 코드가 실행되고 다른 state변화에는 실행되지 않도록 만들자
-// 예) API를 통해 데이터를 가져올 때 컴포넌트 렌더에서 API를 부르고
-// 이후 상태가 변화할 때 그 API에서 데이터를 다시 가져오지 않게 만들 수 있다.
+// useEffect
+// - 두 개의 argument를 가지는 함수
+// - 첫 번째 argument는 우리가 딱 한번만 실행하고 싶은 코드
+// - 두 번째는 [] 배열을 넣어줌
+// -> useEffect가 컴포넌트의 첫 번째 렌더 시점에 iRunOnlyOnce 함수 호출
+// 그리고 상태를 변화시키면 iRunOnlyOnce는 호출되지 않음
+// 즉, 한번만 렌더링 됨
+// 단순화 하여 useEffect(() => {
+// console.log("CALL THE API")
+// },[]); 써도 됨
